6880f1616b7273df7e48dd3c200cf92e37c88e41
Abdullah Farhan <19623305@std.stei.itb.ac.id>
2024-12-16 21:38:23 +0700

    feat:add loading info

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,6 +8,7 @@ export default function ZipUploadPage() {
   const [selectedFile, setSelectedFile] = useState<File | null>(null);
   const [fileSize, setFileSize] = useState<number | null>(null);
   const [selectedJsonFile, setSelectedJsonFile] = useState<File | null>(null);
+  const [isLoading, setIsLoading] = useState(false); // tambahkan state untuk loading
   const { toast } = useToast();
 
   const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -64,6 +65,7 @@ export default function ZipUploadPage() {
       });
       return;
     }
+    setIsLoading(true); 
 
     try {
       // Membuat FormData
@@ -102,6 +104,8 @@ export default function ZipUploadPage() {
         description: "Gagal mengupload file",
         variant: "destructive"
       })
+    } finally {
+      setIsLoading(false); // set loading state ke false setelah upload selesai
     }
   };
 
@@ -175,10 +179,20 @@ export default function ZipUploadPage() {
 
         <Button 
           onClick={handleUpload}
-          disabled={!selectedFile}
+          disabled={!selectedFile || isLoading} // tambahkan kondisi disabled jika sedang loading
           className="w-full"
         >
-          Upload dan Ekstrak ZIP
+          {isLoading ? (
+            <div className="flex justify-center">
+              <svg className="animate-spin h-5 w-5 mr-3" viewBox="0 0 24 24">
+                <circle className="opacity-25" cx="12" cy="12" r="10" fill="currentColor" />
+                <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z" />
+              </svg>
+              Mengunggah...
+            </div>
+          ) : (
+            'Upload dan Ekstrak ZIP'
+          )}
         </Button>
         <h1 className="text-2xl mb-4">Upload File JSON</h1>
         <Input 
